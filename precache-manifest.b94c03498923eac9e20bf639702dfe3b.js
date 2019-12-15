5ce3fd4aa975566811e997712446c44610ea82db
SalihZinaty <salihzinaty@gmail.com>
2019-12-15 11:58:20 +0200

    Updates

---
+++ precache-manifest.b94c03498923eac9e20bf639702dfe3b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26e3c9847f90b21bd52f658e1e2c7eec",
+    "url": "/robofrient/index.html"
+  },
+  {
+    "revision": "b77b6743426717f7caa2",
+    "url": "/robofrient/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "cef83512b6857da6a7be",
+    "url": "/robofrient/static/css/main.b151609f.chunk.css"
+  },
+  {
+    "revision": "b77b6743426717f7caa2",
+    "url": "/robofrient/static/js/2.f9b2e496.chunk.js"
+  },
+  {
+    "revision": "cef83512b6857da6a7be",
+    "url": "/robofrient/static/js/main.4f0e2dd5.chunk.js"
+  },
+  {
+    "revision": "e4a57b0412607bd01b39",
+    "url": "/robofrient/static/js/runtime-main.62b58582.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofrient/static/media/SEGA.12a3db8b.woff"
+  }
+]);
